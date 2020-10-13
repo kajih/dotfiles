db2fdb11e3e246c913204ef21215e1eaf82ab8d7
KaJIH <kajih@home>
2020-10-13 09:51:31 +0200

    font update

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -6,12 +6,14 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 apt update
-curl -L https://download.jetbrains.com/fonts/JetBrainsMono-2.001.zip -o /tmp/monofont.zip
+# curl -L https://download.jetbrains.com/fonts/JetBrainsMono-2.001.zip -o /tmp/monofont.zip
+curl -L https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip -o /tmp/monofont.zip
 
 apt install -y fonts-noto-color-emoji unifont
 dpkg-reconfigure fontconfig-config
 rm /etc/fonts/conf.d/70-no-bitmaps.conf
 
-unzip /tmp/monofont.zip -d /usr/share/fonts
+# unzip /tmp/monofont.zip -d /usr/share/fonts
+unzip /tmp/monofont.zip -d /usr/share/fonts/truetype/nerd/
 fc-cache -f -v
 
