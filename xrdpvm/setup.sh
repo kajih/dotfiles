c2eed8a49c6f3fa4104788c0b6697d42511eb404
kajih <kajih@vm>
2020-10-18 13:50:24 +0200

    xrdp

---
+++ xrdpvm/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt update && sudo apt -y install xrdp i3 i3lock-fancy i3status
+sudo cp /etc/xrdp/xrdp.ini /etc/xrdp/xrdp.ini.bak
+sudo sed -i 's/3389/3390/g' /etc/xrdp/xrdp.ini
+sudo sed -i 's/max_bpp=32/#max_bpp=32\nmax_bpp=128/g' /etc/xrdp/xrdp.ini
+sudo sed -i 's/xserverbpp=24/#xserverbpp=24\nxserverbpp=128/g' /etc/xrdp/xrdp.ini
+echo 'echo $(date) > ~/xsession.log' > ~/.xsession
+
