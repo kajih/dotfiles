5e1bbc7e295f9cc01f8b59368159b8de1572d7f1
KaJIH <kajih@linux>
2021-01-06 17:34:59 +0100

    arch branch

--- scripts/prereq.sh
+++ scripts/prereq.sh
@@ -5,18 +5,7 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-apt update
-apt install -y curl
-
-curl -sL https://deb.nodesource.com/setup_current.x | sudo -E bash -
-
-add-apt-repository -y -n ppa:mmstick76/alacritty
-add-apt-repository -y -n ppa:neovim-ppa/unstable
-add-apt-repository -y -u ppa:longsleep/golang-backports
-
-apt install -y nodejs alacritty neovim golang-go zsh fonts-firacode \
-  tmux neofetch ssh python3-pip 
-
+pacman -Sy --noconfirm curl nodejs go alacritty neovim zsh ttf-fira-code neofetch python-pip tmux npm
 python3 -m pip install --upgrade pynvim
 npm install -g neovim
 npm install -g yarn
