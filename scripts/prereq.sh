f4ca3751d8cb4e4822a011790fbac278aae447bb
kajih <kajih@xub2>
2021-05-02 18:27:46 +0200

    compe and prereqs

--- scripts/prereq.sh
+++ scripts/prereq.sh
@@ -10,7 +10,11 @@ apt install -y curl
 
 curl -sL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 
-add-apt-repository -y -n ppa:mmstick76/alacritty
+# add-apt-repository -y -n ppa:mmstick76/alacritty
+# Hirsuite not having a ppa yet
+echo "deb http://ppa.launchpad.net/mmstick76/alacritty/ubuntu groovy main" > /etc/apt/sources.list
+echo "deb-src http://ppa.launchpad.net/mmstick76/alacritty/ubuntu groovy main" > /etc/apt/sources.list
+
 add-apt-repository -y -n ppa:neovim-ppa/unstable
 add-apt-repository -y -u ppa:longsleep/golang-backports
 
